8fa88dfab5bd16341448dab95be9846fedcb943a
Jaswanth-Chandana <69111830+Jaswanth-Chandana@users.noreply.github.com>
2023-08-14 13:12:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,5 +6,3 @@ streamlit.text('🥣 Omega 3 and Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
-
-
